6c1ff48d06a8e0c2604a061d141bcf242369d90e
rNom <reo1311090912@gmail.com>
2023-01-15 20:53:21 +0900

    correct 012.dart

--- Mathematics_and_Algorithm/012.dart
+++ Mathematics_and_Algorithm/012.dart
@@ -2,22 +2,17 @@
 
 
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   var N = int.parse(stdin.readLineSync()!);
-  
-  print(primejudge(N) ? 'Yes': 'No');
-  
-}
-
-bool primejudge(int n) {
   var flag = true;
-
-  for(int i=2; i<n; i++){
-    if(n % i == 0){
+  
+  for(int i=2; i<sqrt(N).toInt(); i++){
+    if(N % i == 0){
       flag = false;
+      break;
     }
   }
-
-  return flag;
+  print((flag) ? 'Yes' : 'No');
 }
