30355f0229ffa57a0ea7b8519c619ded2007a565
rNom <reo1311090912@gmail.com>
2023-01-31 18:22:15 +0900

    add file

--- AtCoder_Beginners_Selection/ABC086C.dart
+++ AtCoder_Beginners_Selection/ABC086C.dart
@@ -1,14 +1,35 @@
 //Traveling
 
+import 'dart:convert';
 import 'dart:io';
 
-void main() {
+void main() async {
   var N = int.parse(stdin.readLineSync()!);
-  var t = [];
+  var lines = stdin.transform(utf8.decoder).transform(LineSplitter());
 
-  for(int i=0; i<N; i++){
-    t.add(stdin.readLineSync()!.split(' '));
+  var txy = [];
+  var can = true;
+  var cnt = 1;
+  var prev = [0, 0, 0];
+  var diff = 0;
+  var dist = 0;
+
+  await for(var line in lines) {
+    txy = line.split(" ").map(int.parse).toList();
+    diff = (txy[0]-prev[0]);
+    dist = ((txy[1] - prev[1]).abs() + (txy[2] - prev[2]).abs());
+    if(diff >= dist && diff % 2 == dist % 2) {
+      cnt++;
+      if(cnt > N) {
+        break;
+      }
+    } else {
+      can = false;
+      break;
+    }
   }
+
+  print(can ? "Yes" : "No");
 
   
 }
